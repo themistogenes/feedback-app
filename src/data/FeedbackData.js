f5d2174bb5ab815059c824054febec6bcba1c63f
themistogenes <85630327+themistogenes@users.noreply.github.com>
2023-03-30 12:43:38 -0400

    initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, similique consequuntur fugiat vitae necessitatibus mollitia debitis error tempora ex aliquam et suscipit possimus alias laborum beatae dolor facilis commodi consectetur.'
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, similique consequuntur fugiat vitae necessitatibus mollitia debitis error tempora ex aliquam et suscipit possimus alias laborum beatae dolor facilis commodi consectetur.'
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, similique consequuntur fugiat vitae necessitatibus mollitia debitis error tempora ex aliquam et suscipit possimus alias laborum beatae dolor facilis commodi consectetur.'
+  }
+];
+
+export default FeedbackData;
